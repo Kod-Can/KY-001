b8cfd1b5b8b5c210c2497f6bc4df69e4b9c43efe
Kod-Can <140902989+Kod-Can@users.noreply.github.com>
2023-07-29 18:16:34 +0300

    master

---
+++ KY-001.ino
@@ -0,0 +1,19 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define KY001_ONE_WIRE 3
+OneWire oneWire(KY001_ONE_WIRE);
+DallasTemperature sensors(&oneWire);
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println("KY-001 sicaklik sensoru olcumleri");
+  sensors.begin();
+}
+void loop(void)
+{ 
+  sensors.requestTemperatures();
+  Serial.print("Sicaklik: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.println(" °C");
+  delay(1000);
+}
